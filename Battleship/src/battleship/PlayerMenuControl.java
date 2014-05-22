74404b6cbe3e2f2b6e858f802be7263f254aa7db
Owner <Owner@Outpatient-PC>
2014-05-22 13:47:59 -0600

    Added PlayerMenuView and PlayerMenuControl

---
+++ Battleship/src/battleship/PlayerMenuControl.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package battleship;
+
+/**
+ *
+ * @author KamiAnne Dastrup
+ */
+public class PlayerMenuControl {
+    
+    public PlayerMenuControl() {
+    
+    }
+    
+    public void changeP1Name() {
+        System.out.println();
+        this.displayHelpBorder();
+        System.out.println("\tPlease enter the name of Player 1");
+        displayHelpBorder();
+    }
+    
+    public void changeP2Name() {
+        System.out.println();
+        this.displayHelpBorder();
+        System.out.println("\tPlease enter the name of Player 2");
+        displayHelpBorder();
+    }
+    
+    public void changeCompName() {
+        System.out.println();
+        this.displayHelpBorder();
+        System.out.println("\tPlease enter a new name for the computer player");
+        displayHelpBorder();
+    }
+    
+    public void changeFirstTurn() {
+        System.out.println();
+        this.displayHelpBorder();
+        System.out.println("\tPlease enter which player you would like to go first"
+            + "\n\tor enter 'random' to have the game decide for you.");
+        displayHelpBorder();
+    }
+    
+    public void displayHelpBorder() {
+        System.out.println(
+        "\t*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+        );
+    }
+}
---
+++ Battleship/src/battleship/PlayerMenuView.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package battleship;
+
+import java.util.Scanner;
+/**
+ *
+ * @author KamiAnne Dastrup
+ */
+public class PlayerMenuView {
+    
+    private final static String[][] menuItems = {
+        {"1", "Change Player 1 Name"},
+        {"2", "Change Player 2 Name"},
+        {"C", "Change Computer Name"},
+        {"F", "Change who goes first"},
+        {"Q", "Quit Player Menu"}
+    };
+    
+    //Instance of HelpMenuControl class
+    private PlayerMenuControl playerMenuControl = new PlayerMenuControl();
+    
+    //default constructor
+    public PlayerMenuView() {
+    
+    }
+    
+    //display Player Menu and get user input selection
+    public void getInput() {
+    
+        String command;
+        Scanner inFile = new Scanner(System.in);
+        
+        do {
+            this.display(); //display menu
+            
+            //get command entered
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            switch (command) {
+                case "1":
+                    this.playerMenuControl.changeP1Name();
+                    break;
+                case "2":
+                    this.playerMenuControl.changeP2Name();
+                    break;
+                case "C":
+                    this.playerMenuControl.changeCompName();
+                    break;
+                case "F":
+                    this.playerMenuControl.changeFirstTurn();
+                    break;
+                case "Q":
+                    break;
+                default:
+                    new Battleship().displayError("Invalid command. Please enter a valid command.");
+                    continue;
+            }
+        } while (!command.equals("Q"));
+        
+        return;
+    }
+    
+    //displays player menu
+    public final void display() {
+        System.out.println("\tEnter the letter or number assocaiated with one of the following commands:");
+        
+        for (int i = 0; i < PlayerMenuView.menuItems.length; i++) {
+            System.out.println("\t " + menuItems[i][0] + "\t" + menuItems[i][1]);
+        }
+    }
+}
