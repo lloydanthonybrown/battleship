fb15888583efcd53face2ea07bc743226a16c90d
ckcarroll <ckcarroll@outlook.com>
2014-06-10 19:06:06 -0600

    Update Boat.java

--- Battleship/src/battleship/Boat.java
+++ Battleship/src/battleship/Boat.java
@@ -1,18 +1,51 @@
 package battleship;
 
 public class Boat {
-    
+    int hitcheck;
     //added by ckcarroll
     
    public Boat(){
         
     }
     
-    int rowBoat [] = new rowBoat [1];
-    int Sloop [] = new Sloop [2];
-    int Destroyer [] = new Destroyer [4];
-    int Carrier [] = new Carrier [6];
+    int rowBoat [] = new int [1];
+    int Sloop [] = new int [2];
+    int Destroyer [] = new int [4];
+    int Carrier [] = new int [6];
     
     String model;
     int length;
+    
+    
+    private int isSunk(){
+        
+        //check to see if ships are sunk
+        //added by ckcarroll
+        
+        for (int hit = 0; rowBoat.length > hit; hit++){
+            System.out.println("Row boat Sunk. I pitty the fool.");
+            hitcheck+=hit;
+        }
+        
+        for (int hit = 0; Sloop.length > hit; hit++){
+            System.out.println("Sloop Sunk...nbd, just collateral, right?");
+            hitcheck+=hit;
+        }
+        
+        for (int hit = 0; Destroyer.length > hit; hit++){
+            System.out.println("Destroyer Sunk. That was a hard one, "
+                    + "\n\tmaybe next time don't just that away, m'kay?");
+            hitcheck+=hit;
+        }
+        
+        for (int hit = 0; Carrier.length > hit; hit++){
+            System.out.println("Carrier Sunk. Seriously, dude?");
+            hitcheck+=hit;
+        }
+        
+        
+     return hitcheck;
+    }
+    
+    
 }
