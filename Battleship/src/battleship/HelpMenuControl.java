647933f698500f41742f8d825b663fb57bf9a450
ckcarroll <ckcarroll@outlook.com>
2014-06-13 15:01:18 -0600

    Update HelpMenuControl.java

--- Battleship/src/battleship/HelpMenuControl.java
+++ Battleship/src/battleship/HelpMenuControl.java
@@ -5,26 +5,43 @@ public class HelpMenuControl {
       public HelpMenuControl() {
         
     } 
+      
+      
+    public void displayWelcomeHelp(){
+	System.out.println();
+	this.displayHelpBorder();
+	System.out.println(
+	"/tWelcome to the Battleship help menu! Please choose from "
+		+ "the following"
+		+ "options."
+	);
+    }  
 
-    public void displayGridHelp() {
+    public void displayInsHelp() {
         System.out.println();
         this.displayHelpBorder();             
         System.out.println( 
-                "\tBattleship has a grid of 30x30 "
-                + "\n\t You will guess the position of your opponents ship by  "
-                + "\n\t clicking on one square on the grid. "
+                "\tBattleship has a grid of 30x30, 20x20, or 10x10 "
+                + "\nYou will guess the position of your opponents ship by  "
+                + "\nclicking on one square on the grid. When you have hit "
+		+ "\nand sunk all of your opponents ships, then you will "
+		+ "\nwon the game. However, if your opponent sinks all of your"
+		+ "\nships, then you will have lost. "
                 + "\n\t УДАЧИ!");
         displayHelpBorder();
     }
     
     
         
-    public void displayGameHelp() {
+    public void displayObHelp() {
         System.out.println();
         displayHelpBorder();     
         System.out.println( 
                  "\t The objective is to sink your opponents battleship before "
-                + "\n\t they lay waste to your fleet. "
+                + "\n\t they lay waste to your fleet."
+		+ "\n"
+		+ "\n\t If you are experiencing difficulty running the "
+		+ "\ngame, then restart the game or refer to the manual."
                 + "\n\t ОГОНЬ! "
                 ); 
         displayHelpBorder();
@@ -55,7 +72,8 @@ public class HelpMenuControl {
         System.out.println();
         displayHelpBorder();     
         System.out.println( 
-               "\tA location on the board where a player can place their marker."
+               "\tA location on the board where a player can place their marker"
+		       + "\nor where he or she has placed their ships."
                 ); 
         displayHelpBorder();
     }
