983f628613ab468a71f8671fc2dbe395c0acd026
ckcarroll <ckcarroll@outlook.com>
2014-06-25 19:03:28 -0600

    Update MainSuper.java

--- Battleship/src/battleship/MainSuper.java
+++ Battleship/src/battleship/MainSuper.java
@@ -31,5 +31,42 @@ public class MainSuper {
         
     }
     
+     
+    public String executeCommands(){
+        return null;
+    }
     
+public abstract class Menu{
+    
+    public final String [][] menuItems=null;
+    
+    public Menu(){
+        
+    }
+    
+    public Menu(String [][] menuItems){
+        
+    }
+    
+    public String[][] getMenuItems(){
+        return null;
+    }
+    
+    public void setMenuItems(String[][] menuItems){
+        
+    }
+    
+    protected final void display(){
+        
+    }
+    
+    private boolean validCommand(String command){
+        return false;
+    }
+    
+    protected final String getCommand(){
+        return null;
+    }
+    
+}
 }
