2eb5088ee9b0595daab3f0d91fa93ffca41b35ff
ckcarroll <ckcarroll@outlook.com>
2014-07-05 15:25:21 -0400

    test commit

---
+++ Battleship/src/battleship/player/Player1Sub.java
@@ -0,0 +1,18 @@
+package battleship.player;
+
+import battleship.PlayerSuper;
+
+/**
+ *
+ * @author ckcarroll
+ */
+public class Player1Sub extends PlayerSuper{
+    
+    public Player1Sub(){
+        
+    }
+
+   
+    
+    
+}
